6245c347363ea15476e86184312f0365667a2d71
JonasManley <54889189+JonasManley@users.noreply.github.com>
2020-01-24 00:44:42 +0100

    Login implemented

---
+++ InstagramBot/InstaBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace InstaBot
+{
+    class Program
+    {
+
+        static string url = "https://www.instagram.com/accounts/login/?source=auth_switcher";
+
+        static string username = "peterpedersen_1996";
+        static string password = "Test12345";
+
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(200);
+            driver.FindElement(By.Name("username")).SendKeys(username);
+            Thread.Sleep(200);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            Thread.Sleep(200);
+
+            driver.FindElement(By.CssSelector("button[class='sqdOP  L3NKy   y3zKF     ']")).Click();
+
+        }
+    }
+}
---
+++ InstagramBot/Instagram_Bot/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Instagram_Bot")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Instagram_Bot")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95efa62e-7932-4347-bca8-96c1eb0b98ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
